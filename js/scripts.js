2159e4d4c3ddbc3d6ff6fc57b7b909769575d59a
Ashe Urban <ashe@goldentongue.com>
2022-06-10 12:49:29 -0700

    WIP combining range and beep functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,8 @@ function beepBoop(number) {
   let numberArray = [];
   for (let index = 0; index <= parseInt(number); index += 1) {
     numberArray.push(index);
-  }
-  if (numberArray.includes(1)) {
-    return "Beep!";
+    if (numberArray.includes(1) //make this index === "Beep!") {
+    }
   }
   return numberArray;
 }
