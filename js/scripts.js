1295700f6ecf5c5af35b38aa129e0eebac61bbcb
Ashe Urban <ashe@goldentongue.com>
2022-06-10 11:51:28 -0700

    WIP includes 1 return beep.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 function beepBoop(number) {
   let numberArray = [];
   for (let index = 0; index <= parseInt(number); index += 1) {
+    if(index.includes(1))
     numberArray.push(index);
   }
   return numberArray;
