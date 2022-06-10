4d69e7d54cd1fdbde9e3a81691b8dd856defdf9a
Ashe Urban <ashe@goldentongue.com>
2022-06-10 11:21:38 -0700

    UPDATE function to return an array equal to the length of the users number input.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 
 function beepBoop(number) {
   let numberArray = [];
-  for (let index = 0; index < number.length; index += 1) {
-    numberArray.push(number);
+  for (let index = 0; index <= parseInt(number); index += 1) {
+    numberArray.push(index);
   }
   return numberArray;
 }
